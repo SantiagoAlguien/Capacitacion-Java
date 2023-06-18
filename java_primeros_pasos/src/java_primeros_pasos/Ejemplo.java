988b645196b6d4c8e53d5b46b11087b8f6598152
SantiagoAlguin <28.snll.05@gmail.com>
2023-06-17 22:32:12 -0500

    Tipos de variables en java

---
+++ java_primeros_pasos/src/java_primeros_pasos/Ejemplo.java
@@ -0,0 +1,13 @@
+package java_primeros_pasos;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		
+		double variable1 = 323.23;
+		int variable2 = (int) variable1;
+		
+		System.out.println(variable2);
+	}
+
+}
